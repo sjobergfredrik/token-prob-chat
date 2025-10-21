ac2f6bd9b5714d4d34a5c9cdf9ec1875d247b815
Fredrik Sjöberg <fredrik@andhumans.se>
2025-10-21 15:11:05 +0200

    Fix .gitignore to properly exclude node_modules and build artifacts

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(axios)/)', // allow axios to be transformed
-  ],
-};
